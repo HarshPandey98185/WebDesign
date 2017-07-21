5cba642d5d28e6e286761b4596ccad10b54c0838
Harsh Pandey <Pandey.ha@husky.neu.edu>
2017-07-21 19:07:18 -0400

    Travel Wizard Harsh Commit 1

---
+++ Project/Scripts/script.js
@@ -0,0 +1,25 @@
+
+
+var app = angular.module('Demo',['ngRoute']);
+
+app.config(function($routeProvider){
+
+$routeProvider.when("/home",{
+
+                    templateUrl:"Templates/home.html"
+					
+					
+					})
+
+
+
+
+
+
+
+
+
+
+
+})
+     
